ed11187436286b8301de6eb7d40da1d69924cb0d
Marcel Hellkamp <marc@gsites.de>
2017-07-22 10:10:21 +0200

    fix: Travis-CI needs an apt-get update before installing stuff.

--- test/travis_setup.sh
+++ test/travis_setup.sh
@@ -1,19 +1,22 @@
+#!/usr/bin/env bash
 
 # This script is run by travis-ci prior to running tests.
 set -e
 set -x
 
 pip install -U coverage
-pip install -U markerlib # Fix Ubuntu 12.04 "ImportError: No module named _markerlib"
+pip install -U markerlib # Fix Ubuntu "ImportError: No module named _markerlib"
 pip install coveralls
 
 # Server back-ends and template engines. Not all back-ends support all python versions and we only want to test for 2.7 and 3.6 to keep things sane
 if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then
-    sudo apt-get install libev-dev
+    sudo apt-get update -y
+    sudo apt-get install -y libev-dev
     pip install mako jinja2 waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld gunicorn eventlet
     pip install flup fapws3 rocket bjoern gevent
 elif  [[ $TRAVIS_PYTHON_VERSION == 3.6 ]]; then
-    sudo apt-get install libev-dev
+    sudo apt-get update -y
+    sudo apt-get install -y libev-dev
     pip install mako jinja2 waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld gunicorn eventlet
     pip install uvloop
 fi
